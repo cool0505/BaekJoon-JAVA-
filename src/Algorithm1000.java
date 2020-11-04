eb02a8bb3b0a5316b6714a9f815c735aae45ac12
cool0505 <joowonkoh@naver.com>
2020-11-04 23:13:58 +0900

    edit 1000

--- src/Algorithm1000.java
+++ src/Algorithm1000.java
@@ -3,14 +3,10 @@ import java.util.Scanner;
 public class Algorithm1000 {
  
 	public static void main(String[] args) {
- 
 		Scanner in = new Scanner(System.in);
 		int A = in.nextInt();
-		int B = in.nextInt();
-		
+		int B = in.nextInt();	
 		System.out.println(A+B);
- 
-		in.close();
-		
+		in.close();	
 	}
 }
